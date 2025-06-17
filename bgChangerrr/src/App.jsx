68e9ccae006a634865f6392d3bf32b4b27106603
miskogepa <milovan.jankovic50m@gmail.com>
2025-06-17 22:40:01 +0200

    Initialize bgChangerrr project with React, Vite, and Tailwind CSS setup
    - Created package.json with dependencies and scripts for development, build, linting, and preview.
    - Added Vite logo SVG to public directory.
    - Created App component with basic layout and buttons.
    - Included React logo SVG in assets.
    - Set up global CSS with Tailwind CSS import.
    - Implemented main entry point for React application.
    - Configured Vite with React and Tailwind CSS plugins.

---
+++ bgChangerrr/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="bg-amber-300 w-full h-screen duration-200 rounded-4xl ">
+      <div className=" fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button className=" py-2 roundedfont-bold uppercase cursor-pointer">
+            test
+          </button>
+           <button className=" py-2 roundedfont-bold uppercase cursor-pointer">
+            test
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
