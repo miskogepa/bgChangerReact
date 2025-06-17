ba7f0d23cec820742d196f54f9def29f75167465
miskogepa <milovan.jankovic50m@gmail.com>
2025-06-17 22:56:54 +0200

    Refactor color change buttons to directly use setColor and remove unused changeColor function

--- bgChangerrr/src/App.jsx
+++ bgChangerrr/src/App.jsx
@@ -7,18 +7,18 @@ function App() {
  const [color, setColor] = useState("olive"); 
  // koristimo hook useState za upravljanje stanjem boje
 
- function changeColor(color) {
-  setColor(color)
- }
+//  function changeColor(color) {
+//   setColor(color)
+//  }
 
   return (
     <div className="bg-amber-300 w-full h-screen duration-200 rounded-4xl " style={{ backgroundColor: color }}>
       <div className=" fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-          <button onClick={() => changeColor("red") } className=" py-2 roundedfont-bold uppercase cursor-pointer">
+          <button onClick={() => setColor("red") } className=" py-2 roundedfont-bold uppercase cursor-pointer bg-red-500">
             red
           </button>
-           <button onClick={() => changeColor("blue") } className=" py-2 roundedfont-bold uppercase cursor-pointer">
+           <button onClick={() => setColor("blue") } className=" py-2 roundedfont-bold uppercase cursor-pointer bg-blue-600">
             blue
           </button>
         </div>
