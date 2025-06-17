c7db009b15682a7fe218e7567b9504152bc2c5a3
miskogepa <milovan.jankovic50m@gmail.com>
2025-06-17 22:50:06 +0200

    Refactor App component to manage background color state using useState hook

--- bgChangerrr/src/App.jsx
+++ bgChangerrr/src/App.jsx
@@ -4,10 +4,10 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+ const [color, setColor] = useState("olive"); // koristimo hook useState za upravljanje stanjem boje
 
   return (
-    <div className="bg-amber-300 w-full h-screen duration-200 rounded-4xl ">
+    <div className="bg-amber-300 w-full h-screen duration-200 rounded-4xl " style={{ backgroundColor: color }}>
       <div className=" fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button className=" py-2 roundedfont-bold uppercase cursor-pointer">
