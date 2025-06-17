9b00e4b9b15d7de922ed9537f2d47de47ad64812
miskogepa <milovan.jankovic50m@gmail.com>
2025-06-17 22:54:34 +0200

    Refactor color change functionality in App component and remove test buttons

--- bgChangerrr/src/App.jsx
+++ bgChangerrr/src/App.jsx
@@ -4,17 +4,22 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
- const [color, setColor] = useState("olive"); // koristimo hook useState za upravljanje stanjem boje
+ const [color, setColor] = useState("olive"); 
+ // koristimo hook useState za upravljanje stanjem boje
+
+ function changeColor(color) {
+  setColor(color)
+ }
 
   return (
     <div className="bg-amber-300 w-full h-screen duration-200 rounded-4xl " style={{ backgroundColor: color }}>
       <div className=" fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-          <button className=" py-2 roundedfont-bold uppercase cursor-pointer">
-            test
+          <button onClick={() => changeColor("red") } className=" py-2 roundedfont-bold uppercase cursor-pointer">
+            red
           </button>
-           <button className=" py-2 roundedfont-bold uppercase cursor-pointer">
-            test
+           <button onClick={() => changeColor("blue") } className=" py-2 roundedfont-bold uppercase cursor-pointer">
+            blue
           </button>
         </div>
       </div>
